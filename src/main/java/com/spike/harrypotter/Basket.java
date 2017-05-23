ce17f23b154db05e09ba98dc621c1f9abe89631a
Pietro Di Bello <pietro.dibello@xpeppers.com>
2017-05-23 23:15:24 +0200

    Can compute the cost of a basket made only of copies of the same Harry Potter book in the series

--- src/main/java/com/spike/harrypotter/Basket.java
+++ src/main/java/com/spike/harrypotter/Basket.java
@@ -9,8 +9,12 @@ public class Basket {
     private Map<String, Integer> collection = new HashMap<>();
 
     public long cost() {
-        long price = collection.getOrDefault("Harry Potter I", 0) * PRICE_IN_CENTS;
-        return price;
+        Integer first = collection.getOrDefault("Harry Potter I", 0);
+        Integer second = collection.getOrDefault("Harry Potter II", 0);
+        Integer third = collection.getOrDefault("Harry Potter III", 0);
+        Integer fourth = collection.getOrDefault("Harry Potter IV", 0);
+        Integer fifth = collection.getOrDefault("Harry Potter V", 0);
+        return (first + second + third + fourth + fifth) * PRICE_IN_CENTS;
     }
 
     public void add(int copies, String title) {
--- src/test/java/com/spike/harrypotter/BasketTest.java
+++ src/test/java/com/spike/harrypotter/BasketTest.java
@@ -3,6 +3,9 @@ package com.spike.harrypotter;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class BasketTest {
@@ -15,14 +18,36 @@ public class BasketTest {
     }
 
     @Test
-    public void emptyBasketCostsNothing() throws Exception {
-        assertEquals(0L, basket.cost());
+    public void empty_basket_costs_nothing() throws Exception {
+        assertEquals(0L, new Basket().cost());
     }
 
     @Test
-    public void oneBookCosts8Euros() throws Exception {
+    public void one_book_costs_8_euros() throws Exception {
         basket.add(1, "Harry Potter I");
 
         assertEquals(800L, basket.cost());
+    }
+
+    @Test
+    public void more_copies_of_the_same_book_got_no_discount() throws Exception {
+        basket.add(4, "Harry Potter I");
+
+        assertEquals(4 * 800L, basket.cost());
+    }
+
+    @Test
+    public void any_single_book_of_the_series_costs_8_euros() throws Exception {
+        List<String> numbers = asList("I", "II", "III", "IV", "V");
+        numbers.stream().forEach((n) -> {
+            Basket b = basketWithOneBookOfSeries(n);
+            assertEquals(800L, b.cost());
+        });
+    }
+
+    private Basket basketWithOneBookOfSeries(String series) {
+        Basket basket = new Basket();
+        basket.add(1, "Harry Potter " + series);
+        return basket;
     }
 }
